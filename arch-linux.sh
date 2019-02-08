157e9564dc6549da5a2590f3d7810694c6845404
Markus Pesch <markus.pesch@cryptic.systems>
2019-02-08 19:30:24 +0100

    fix: arch-linux installation script

--- arch-linux.sh
+++ arch-linux.sh
@@ -12,15 +12,22 @@ ROOT="${DEVICE}2"
 # Arch Linux Image
 TARBALL=ArchLinuxARM-rpi-latest.tar.gz
 TARBALL_SIG="${TARBALL}.sig"
-TARBALL_SOURCE=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
+TARBALL_SOURCE=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz
 TARBALL_SOURCE_SIG="${TARBALL_SOURCE}.sig"
+TARBALL_SIG_KEY="68B3537F39A313B3E574D06777193F152BDBE6A6"
 
 # download tar
-curl --location $TARBALL_SOURCE --output $TARBALL
-curl --location $TARBALL_SOURCE_SIG --output $TARBALL_SIG
+if [ ! -f ${TARBALL} ]; then
+  curl --location ${TARBALL_SOURCE} --output ${TARBALL}
+fi
+
+if [ ! -f ${TARBALL_SIG} ]; then
+  curl --location ${TARBALL_SOURCE_SIG} --output ${TARBALL_SIG}
+fi
 
 # check gpg
-gpg --verify $TARBALL_SIG $TARBALL
+# gpg --recv-keys ${TARBALL_SIG_KEY}
+# gpg --verify ${TARBALL_SIG} ${TARBALL}
 
 # delete partitions
 for p in $(parted -s $DEVICE print|awk '/^ / {print $1}'); do
@@ -32,8 +39,8 @@ parted --script $DEVICE mkpart primary fat32 1MiB 100MiB
 parted --script $DEVICE mkpart primary ext4  100Mib 100%
 
 # file systems
-mkfs.vfat $BOOT -L boot
-mkfs.ext4 $ROOT -L root
+mkfs.vfat ${BOOT}
+mkfs.ext4 ${ROOT} -L root
 
 # mount file systems
 if [ ! -d boot ]; then
@@ -44,27 +51,55 @@ if [ ! -d root ]; then
   mkdir root
 fi
 
-mount $BOOT ./boot
-mount $ROOT ./root
-
+mount ${BOOT} ./boot
+mount ${ROOT} ./root
 
 # extract tar
-tar --extract --gunzip --same-permissions --file $PI_IMAGE --directory ./root
+tar --extract --gunzip --same-permissions --directory="./root" --file ${TARBALL}
 sync
 
 # move bootloader
 mv ./root/boot/* ./boot
 
 # install ssh pub key
-mkdir ./root/root/.ssh
 cat > ./root/root/.ssh/authorized_keys <<EOF
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPOydCxv9/tAV7AdS2HsUIEu547Z5qUJnWYwiO7rI9YL markus-pc
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUTcUBb+55jRY9TkpLgm8K/8nJfEXyjEX8zljdCCRpi markus-nb
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVGxeVfkycwzP7UkLujGzDjC+9lPML45V7+bBmkKyD0 backup
 EOF
-chown root: ./root/root/.ssh/authorized_keys
+
+chown root:root -R ./root/root/.ssh/authorized_keys
 chmod 640 ./root/root/.ssh/authorized_keys
 
+# set hosts
+cat > ./root/etc/hosts <<EOF
+127.0.0.1       localhost
+127.0.1.1       localhost.localdomain localhost
+
+# The following lines are desirable for IPv6 capable hosts
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOF
+
+
+
+# default bashrc
+cat > ./root/root/.bashrc <<"EOF"
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Bash settings
+shopt -s globstar
+shopt -s histappend
+
+# Aliases
+uap='pacman --sync --sysupgrade --refresh'
+
+export PS1='\u@\h:\w$(__git_ps1 " (%s)")\$ '
+EOF
+
+
 # umount partitions and remove old files
-umount $BOOT $ROOT
-rm -Rf ./boot ./root $PI_IMAGE
+umount ${BOOT} ${ROOT}
+rm -Rf ./boot ./root ${TARBALL} ${TARBALL_SIG}
